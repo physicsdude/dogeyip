aa4a04025e3682ccbff5ed0a4f2bb99cd2455fae
George Carlson <gcarlson@adc4gis.com>
2015-01-04 09:49:09 -0600

    Fix: html class should not have profilelink

--- js/doge_yip_listaccount.js
+++ js/doge_yip_listaccount.js
@@ -2,7 +2,7 @@ function listAccount(accountAddress){
   var qrcode = "https://dogechain.info/api/v1/address/qrcode/"+accountAddress;
   var profile = "profile.html?user="+accountAddress;
   var result = "<div class='container'>"
-               + "<a class='profilelink' href='"+profile+"'>"
+               + "<a href='"+profile+"'>"
                + "<img style='vertical-align:middle;' width=35px height=35px src='"+qrcode+"'></img>"
                + "<span><font class="+accountAddress+" style='font-size: 175%; vertical-align:middle;'>&nbsp;"+accountAddress+"</font></span>"
                + "</a>"
