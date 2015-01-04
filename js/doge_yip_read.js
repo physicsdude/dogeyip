3ad12004e40d8752dcb36cba8e323f0449d876fb
George Carlson <gcarlson@adc4gis.com>
2015-01-04 04:04:13 -0600

    Update: Display Favorited Posts

--- js/doge_yip_read.js
+++ js/doge_yip_read.js
@@ -15,7 +15,7 @@ function createPost(address, tx, hexMessage, dictionary){
   var user = getUserAddress();
   var name = getUserName();
   var favoriteurl = 'favorite.html?user='+user+'&name='+name+'&favaccount='+favaccount+'&favamount='+favamount;
-  var post = '<div style="padding: 10px">'
+  var post = '<div id="'+tx.time+'" style="padding: 10px">'
              + '<table>'
              +  '<tr>'
              +   '<td><img width=20 height=20 src="https://useiconic.com/iconic/svg/comment-square.svg"/></td>'
@@ -26,12 +26,84 @@ function createPost(address, tx, hexMessage, dictionary){
              +   '<td></td>'
              +   '<td>&nbsp;'
              +    '<a href="'+favoriteurl+'">'
-             +     '<img width=15 height=15 src="https://useiconic.com/iconic/svg/thumb.svg"/> 0<i>'
-             +    '</a><font color="gray"> - '+timestamp(tx.time)+'</font></i>'
+             +     '<img width=15 height=15 src="https://useiconic.com/iconic/svg/thumb.svg"/>'
+             +    '</a><i><font color="gray"> &bull; '+timestamp(tx.time)+'</font></i>'
              +   '</td>'
              +  '</tr>'
              + "</table></div>";
-  $("#posts").append(post);
+  var innerDiv = null;
+  $('#posts').find('div').each(function(){
+    var innerDivId = $(this).attr('id');
+    if(innerDivId>tx.time && (innerDiv==null || innerDiv.attr('id')>innerDivId)){
+      innerDiv=$(this);
+    }
+  });
+  if(innerDiv==null){
+    $("#posts").prepend(post);
+  } else{
+    innerDiv.after(post);
+  }
+}
+
+function createFavorite(favamount, favaccount, address, tx, hexMessage, dictionary){
+  var favname = favaccount;
+  var user = getUserAddress();
+  var name = getUserName();
+  var url = "https://chain.so/api/v2/address/DOGE/"+favaccount;
+  var favoriteurl = 'favorite.html?user='+favaccount+'&name='+favaccount+'&favaccount='+favaccount+'&favamount='+favamount;
+  $.getJSON(url, function(json) {
+    for(var i=0; i<json.data.txs.length; i++){
+      var tx = json.data.txs[i];
+      if(tx.outgoing!=null){
+        for(j=0; j<tx.outgoing.outputs.length; j++){
+          var output = tx.outgoing.outputs[j];
+          var hash160 = base58CheckTohash160(output.address);
+          var hexMessage = hash160.substring(0,38);
+          var hexToken = parseInt(hash160.substring(38,40), 16);
+          if(isPost(hexToken) && favamount==(tx.time/100000000)){
+            var post = '<div id="'+tx.time+'" style="padding: 10px">'
+              + '<table>'
+              +  '<tr>'
+              +   '<td><img width=20 height=20 src="https://useiconic.com/iconic/svg/star.svg"/></td>'
+              +   '<td>&nbsp;<a href="profile.html?user='+favaccount+'"><font class="'+favaccount+'">'+favname+'</font></a></td>'
+              +  '</tr>'
+              +  '<tr><td></td><td>&nbsp;'+hash160ToText(hexMessage, dictionary)+'</td></tr>'
+              +  '<tr>'
+              +   '<td></td>'
+              +   '<td>&nbsp;'
+              +    '<a href="'+favoriteurl+'">'
+              +     '<img width=15 height=15 src="https://useiconic.com/iconic/svg/thumb.svg"/>'
+              +    '</a><i><font color="gray"> &bull; '+timestamp(tx.time)+'</font></i>'
+              +   '</td>'
+              +  '</tr>'
+              + "</table></div>";
+            var innerDiv = null;
+            $('#posts').find('div').each(function(){
+              var innerDivId = $(this).attr('id');
+              if(innerDivId>tx.time && (innerDiv==null || innerDiv.attr('id')>innerDivId)){
+                innerDiv=$(this);
+              }
+            });
+            if(innerDiv==null){
+              $("#posts").prepend(post);
+            } else{
+              innerDiv.after(post);
+            }
+          };
+          if(isName(hexToken)){
+            favname = hash160ToText(hexMessage, dictionary).trim();
+          }
+        }
+      }
+    };
+    $("."+favaccount).text(" "+favname);
+  });
+}
+
+function isFavorite(tx, output){
+  var amount = output.value
+  var time = tx.time/100000000;
+  return (amount>time-1 && amount<time+1);
 }
 
 function isPost(hexToken){
@@ -61,10 +133,13 @@ function scrapeTransactionData(userAddress){
             var hexMessage = hash160.substring(0,38);
             var hexToken = parseInt(hash160.substring(38,40), 16);
             if(isPost(hexToken)){
-              createPost(userName, tx, hexMessage, dictionary)
+              createPost(userName, tx, hexMessage, dictionary);
             }
             if(isName(hexToken)){
               userName = hash160ToText(hexMessage, dictionary).trim();
+            }
+            if(isFavorite(tx, output)){
+              createFavorite(output.value, output.address, userName, tx, hexMessage, dictionary);
             }
           }
         }
